28a9d6e988400e16a0fe477c4e111f3c841cbff1
ernesto <bossi.ernestog@gmail.com>
2013-12-29 16:32:43 -0300

    moved to version 1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="uLisp",
-    version="0.1",
+    version="0.1.2",
     description="Very tiny Lisp interpreter",
     author="Ernesto Bossi",
     author_email="bossi.ernestog@gmail.com",
